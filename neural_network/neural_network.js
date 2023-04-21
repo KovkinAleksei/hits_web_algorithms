54df1b830536497863283f291bb52c675acd4cdf
sitdi <sitdikov.yuriy@gmail.com>
2023-04-21 12:10:34 +0700

    Code refactoring.

--- neural_network/neural_network.js
+++ neural_network/neural_network.js
@@ -57,23 +57,16 @@ let ctx = cnv.getContext("2d");
 
 let imgWidth = canvas.width;
 let imgHeight = canvas.height;
-// Функция запускающая работу нн
-function neuralNetwork() {
-    let canvas = document.getElementById('drawField');
-    let image = new Image();
-    image.src = canvas.toDataURL();
 
-    image.onload = () => {
-        var imgData = context.getImageData(0, 0, imgWidth, imgHeight).data;
-        console.log(imgData)
-        let test = new Array(imgHeight * imgWidth); 
+function imageCenterig(imgData) {
+    let test = new Array(imgHeight * imgWidth); 
         for (let i = 3; i < imgData.length; i += 4) {
             test[ Math.floor(i / 4) ] = (imgData[i] / 255);
         }
 
 
 
-        // get the corners of the content (not the spaces)
+        // найти границы цифры
         let cropTop = scanY(true, test);
         let cropBottom = scanY(false, test);
         let cropLeft = scanX(true, test);
@@ -81,28 +74,40 @@ function neuralNetwork() {
 
         let cropXDiff = cropRight - cropLeft;
         let cropYDiff = cropBottom - cropTop;
-        console.log(cropTop +  " " + cropLeft + " " + cropBottom + " " + cropRight)
-        // get the content that doesn't contain outer spaces
+        // console.log(cropTop +  " " + cropLeft + " " + cropBottom + " " + cropRight)
 
         cnv.width = 28;
         cnv.height = 28;
 
+        // Просчитывание паддингов чтобы цифра была не во весь канвас
         let size = Math.floor(Math.max(cropXDiff, cropYDiff) * 0.3) * 2 + Math.max(cropXDiff, cropYDiff);
         let gorizontalPaddings = Math.floor((size - cropXDiff) / 2);
         let verticalPaddings = Math.floor((size - cropYDiff) / 2);
 
+        // Нарисовать итоговое изображение
         ctx.drawImage(image, cropLeft - gorizontalPaddings, cropTop - verticalPaddings, cropXDiff + (gorizontalPaddings * 2), cropYDiff + (verticalPaddings * 2), 0, 0, 28, 28);
-        let scaledImage = ctx.getImageData(0, 0, 28, 28).data;
+        return ctx.getImageData(0, 0, 28, 28).data;
+}
 
-        //let scaledImage = scaleImageData(image);
+// Функция запускающая работу нн
+function neuralNetwork() {
+    let canvas = document.getElementById('drawField');
+    let image = new Image();
+    image.src = canvas.toDataURL();
+
+    image.onload = () => {
+        // Получить данные с канваса и центрировать изображение
+        var imgData = context.getImageData(0, 0, imgWidth, imgHeight).data;
+        let scaledImage = imageCenterig(imgData);
+
+        // Преобразовать в одноканальную картинку
         let oneChannelImage = new Array(28 ** 2);
-
         for (let i = 3; i < scaledImage.length; i += 4) {
             oneChannelImage[ Math.floor(i / 4) ] = (scaledImage[i] / 255);
         }
 
-        // let centredImage = imageCentring(oneChannelImage);
         // console.log('centr ' + centredImage)
+        // Запуск прямого прохода и вывод результата
         let resultImage = feedforward(oneChannelImage);
         let max = -1, maxInd = 0;
         for (let i = 0; i < resultImage.length; i++) {
@@ -118,10 +123,7 @@ function neuralNetwork() {
 function scanX(fromLeft, imgData) {
     var offset = fromLeft? 1 : -1;
 
-    // loop through each column
     for(var x = fromLeft ? 0 : imgWidth - 1; fromLeft ? (x < imgWidth) : (x > -1); x += offset) {
-
-        // loop through each row
         for(var y = 0; y < imgHeight; y += 4) {
             let index = (y * imgHeight) + x;
             if (imgData[index] != 0) {
@@ -133,16 +135,13 @@ function scanX(fromLeft, imgData) {
             }      
         }
     }
-    return null; // all image is white
+    return null;
 };
 
 function scanY(fromTop, imgData) {
     var offset = fromTop ? 1 : -1;
 
-    // loop through each row
     for(var y = fromTop ? 0 : imgHeight - 1; fromTop ? (y < imgHeight) : (y > -1); y += offset) {
-
-        // loop through each column
         for(var x = 0; x < imgWidth; x++) {
             let index = (y * imgHeight) + x;
             if (imgData[index] != 0) {
@@ -154,7 +153,7 @@ function scanY(fromTop, imgData) {
             }
         }
     }
-    return null; // all image is white
+    return null;
  }
 
  // Очистить канвас
