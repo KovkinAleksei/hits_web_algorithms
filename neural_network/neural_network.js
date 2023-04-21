974aa38a2f65f4997df0cfccd42e418f3407b532
sitdi <sitdikov.yuriy@gmail.com>
2023-04-21 11:17:10 +0700

    Added image centering.

--- neural_network/neural_network.js
+++ neural_network/neural_network.js
@@ -2,14 +2,14 @@ import { feedforward } from "./nn.js";
 
 // Создание канваса
 let canvas = document.getElementById("drawField");
-let ctx = canvas.getContext("2d");
+let context = canvas.getContext("2d");
 
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
 let brushSize = document.getElementById("brushSize");
-brushSize.addEventListener('change', function() { ctx.lineWidth = brushSize.value; })
+brushSize.addEventListener('change', function() { context.lineWidth = brushSize.value; })
 
-ctx.lineWidth = brushSize.value;
+context.lineWidth = brushSize.value;
 
 // устанавливаем начальные координаты
 let lastX;
@@ -22,8 +22,8 @@ canvas.addEventListener("mousedown", function(e) {
     lastX = e.clientX - canvas.offsetLeft;
     lastY = e.clientY - canvas.offsetTop;
     draw = true;
-    ctx.beginPath();
-    ctx.moveTo(lastX, lastY);
+    context.beginPath();
+    context.moveTo(lastX, lastY);
 });
 
 // обработчик движения мыши
@@ -36,9 +36,9 @@ canvas.addEventListener("mousemove", function(e) {
         let currentX = e.clientX - canvas.offsetLeft;
         let currentY = e.clientY - canvas.offsetTop;
 
-        ctx.lineTo(currentX, currentY);
-        ctx.strokeStyle = "black";
-        ctx.stroke();
+        context.lineTo(currentX, currentY);
+        context.strokeStyle = "black";
+        context.stroke();
 
         lastX = currentX;
         lastY = currentY;
@@ -52,12 +52,11 @@ canvas.addEventListener("mouseup", function(e) {
     neuralNetwork();
 });
 
-// Очистить канвас
-function clearCanvas() {
-    ctx.clearRect(0,0, canvas.width, canvas.height);
-    document.getElementById("answerLabel").textContent = "";
-}
+let cnv = document.getElementById('cnv');
+let ctx = cnv.getContext("2d");
 
+let imgWidth = canvas.width;
+let imgHeight = canvas.height;
 // Функция запускающая работу нн
 function neuralNetwork() {
     let canvas = document.getElementById('drawField');
@@ -65,11 +64,41 @@ function neuralNetwork() {
     image.src = canvas.toDataURL();
 
     image.onload = () => {
-        let scaledImage = scaleImageData(image);
+        var imgData = context.getImageData(0, 0, imgWidth, imgHeight).data;
+        console.log(imgData)
+        let test = new Array(imgHeight * imgWidth); 
+        for (let i = 3; i < imgData.length; i += 4) {
+            test[ Math.floor(i / 4) ] = (imgData[i] / 255);
+        }
+
+
+
+        // get the corners of the content (not the spaces)
+        let cropTop = scanY(true, test);
+        let cropBottom = scanY(false, test);
+        let cropLeft = scanX(true, test);
+        let cropRight = scanX(false, test);
+
+        let cropXDiff = cropRight - cropLeft;
+        let cropYDiff = cropBottom - cropTop;
+        console.log(cropTop +  " " + cropLeft + " " + cropBottom + " " + cropRight)
+        // get the content that doesn't contain outer spaces
+
+        cnv.width = 28;
+        cnv.height = 28;
+
+        let size = Math.floor(Math.max(cropXDiff, cropYDiff) * 0.3) * 2 + Math.max(cropXDiff, cropYDiff);
+        let gorizontalPaddings = Math.floor((size - cropXDiff) / 2);
+        let verticalPaddings = Math.floor((size - cropYDiff) / 2);
+
+        ctx.drawImage(image, cropLeft - gorizontalPaddings, cropTop - verticalPaddings, cropXDiff + (gorizontalPaddings * 2), cropYDiff + (verticalPaddings * 2), 0, 0, 28, 28);
+        let scaledImage = ctx.getImageData(0, 0, 28, 28).data;
+
+        //let scaledImage = scaleImageData(image);
         let oneChannelImage = new Array(28 ** 2);
 
-        for (let i = 3; i < scaledImage.data.length; i += 4) {
-            oneChannelImage[ Math.floor(i / 4) ] = (scaledImage.data[i] / 255);
+        for (let i = 3; i < scaledImage.length; i += 4) {
+            oneChannelImage[ Math.floor(i / 4) ] = (scaledImage[i] / 255);
         }
 
         // let centredImage = imageCentring(oneChannelImage);
@@ -86,122 +115,53 @@ function neuralNetwork() {
     }
 }
 
-function imageCentring(image){
-    let up = 28;
-    let down = 0;
-    let right = 0;
-    let left = 28;
-    for(let i = 0; i < 28; i++) {
-        for(let j = 0; j < 28; j++) {
-            let index = (28 * i) + j;
-            if(image[index] > 0){
-                
-                if(up > i) {
-                    up = i;
+function scanX(fromLeft, imgData) {
+    var offset = fromLeft? 1 : -1;
+
+    // loop through each column
+    for(var x = fromLeft ? 0 : imgWidth - 1; fromLeft ? (x < imgWidth) : (x > -1); x += offset) {
+
+        // loop through each row
+        for(var y = 0; y < imgHeight; y += 4) {
+            let index = (y * imgHeight) + x;
+            if (imgData[index] != 0) {
+                if (fromLeft) {
+                    return x;
+                } else {
+                    return Math.min(x + 1, imgWidth);
                 }
-                if(left > j){
-                    left = j;
-                }
-                if(right < j){
-                    right = j;
-                }
-                if(down < i){
-                    down = i;
+            }      
+        }
+    }
+    return null; // all image is white
+};
+
+function scanY(fromTop, imgData) {
+    var offset = fromTop ? 1 : -1;
+
+    // loop through each row
+    for(var y = fromTop ? 0 : imgHeight - 1; fromTop ? (y < imgHeight) : (y > -1); y += offset) {
+
+        // loop through each column
+        for(var x = 0; x < imgWidth; x++) {
+            let index = (y * imgHeight) + x;
+            if (imgData[index] != 0) {
+                if (fromTop) {
+                    return y;
+                } else {
+                    return Math.min(y + 1, imgHeight);
                 }
             }
         }
     }
-    
-    let vert = (down - up);
-    let goriz = (right - left);
+    return null; // all image is white
+ }
 
-    let helpImage = new Array(goriz);
-for(let i = 0; i < goriz; i++) {
-    helpImage[i] = new Array(vert);
-}
-
-    let ind = 0, jind = 0;
-    for(let i = 0; i < 28; i++) {
-        for(let j = 0; j < 28; j++) {
-            if(i >= up && i < down && j >= left && j < right) {
-                let index = (28 * i) + j;
-                // console.log(index)
-                // console.log('ind jind ' + ind + " " + jind)
-                helpImage[ind][jind] = image[index];
-                jind++;
-                if(jind >= vert){
-                    ind++;
-                    jind = 0;
-                }
-            }
-        }
-    }
-
-    let size = Math.floor(Math.max(vert, goriz) * 0.3) * 2 + Math.max(vert, goriz);
-    let newImage = new Array(size);
-    for(let i = 0; i < size; i++) {
-        newImage[i] = new Array(size).fill(0);
-    }
-    
-    let gorizontalPaddings = Math.floor((size - goriz) / 2);
-    let verticalPaddings = Math.floor((size - vert) / 2);
-    ind = 0; jind = 0;
-    for(let i = 0; i < size; i++) {
-        for(let j = 0; j < size; j++) {
-            if(i > verticalPaddings && i < size - verticalPaddings && j > gorizontalPaddings && j < size - gorizontalPaddings){
-                // console.log(ind + " " + jind)
-                newImage[i][j] = helpImage[ind][jind];
-                jind++;
-                if(jind >= vert){
-                    ind++;
-                    jind = 0;
-                }
-            }
-        }
-    }
-    // console.log(newImage)
-    const cnvas = document.createElement('canvas');
-    cnvas.width = size;
-    cnvas.height = size;
-    const cotx = cnvas.getContext('2d');
-    const imageData = cotx.createImageData(cnvas.width, cnvas.height);
-    for (let y = 0; y < cnvas.height; y++) {
-      for (let x = 0; x < cnvas.width; x++) {
-        const i = (y * cnvas.width + x) * 4;
-        imageData.data[i] = 0;
-        imageData.data[i + 1] = 0;
-        imageData.data[i + 2] = 0;
-        imageData.data[i + 3] = newImage[y][x] * 255; // установка непрозрачности
-      }
-    }
-    console.log(imageData)
-    cotx.putImageData(imageData, 0, 0);
-    
-    let resImage = new Image();
-    resImage.src = cnvas.toDataURL();
-
-    let scaledImage = scaleImageData(resImage);
-    console.log(scaledImage)
-    let oneChannelImage = new Array(28 ** 2);
-
-    for (let i = 3; i < scaledImage.data.length; i += 4) {
-        oneChannelImage[ Math.floor(i / 4) ] = (scaledImage.data[i] / 255);
-    }
-    // console.log(oneChannelImage);
-    return oneChannelImage;
-}
-
-function scaleImageData(image) {
-
-    let canvas = document.createElement("canvas");
-
-    let ctx = canvas.getContext("2d");
-
-    canvas.width = 28;
-    canvas.height = 28;
-
-    ctx.drawImage(image, 0, 0, 28, 28);    
-    return ctx.getImageData(0, 0, 28, 28);
+ // Очистить канвас
+function clearCanvas() {
+    context.clearRect(0,0, canvas.width, canvas.height);
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    document.getElementById("answerLabel").textContent = "";
 }
 
 let clearButton = document.getElementById('clearButton');
